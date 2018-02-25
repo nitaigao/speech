1a3aef26e7e2067240c6db406a194c4c98bd91c6
Nicholas Kostelnik <nkostelnik@gmail.com>
2018-02-25 18:41:43 +0800

    Hotword confirmation first pass

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ SOURCES := $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
 STATIC_LIB := ./lib/snowboy/lib/libsnowboy-detect.a lib/portaudio/install/lib/libportaudio.a
 OBJECTS := $(patsubst $(SRCDIR)/%,$(BUILD_DIR)/%,$(SOURCES:.$(SRCEXT)=.o)) $(STATIC_LIB)
 CFLAGS := -g -Wall -std=c++11 -msse -msse2
-LIB := -pthread -lportaudio -lprotobuf -lgrpc -lgrpc++ -lcblas -lasound -L./lib/libfvad/src/.libs -lfvad
+LIB := -pthread -lsndfile -lportaudio -lprotobuf -lgrpc -lgrpc++ -lcblas -lasound -L./lib/libfvad/src/.libs -lfvad
 INC := -I include -I ./lib/snowboy/include -I lib/portaudio/install/include -I lib/libfvad/include -Ilib/googleapis
 COPY_FILES = common.res snowboy.umdl
 
--- src/main.cc
+++ src/main.cc
@@ -2,12 +2,16 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include <queue>
 #include <time.h>
 
 #include <grpc++/grpc++.h>
 
 #include "portaudiosystem.h"
 #include "hotword.h"
+#include "snowboy-vad-cxx-compat.h"
+
+#include <sndfile.hh>
 
 #include "google/cloud/dialogflow/v2beta1/session.grpc.pb.h"
 
@@ -18,29 +22,28 @@ class SpeechCall {
 
 public:
 
-  SpeechCall(grpc::CompletionQueue* cq, grpc::ClientContext* context, Sessions::Stub* service)
+  SpeechCall(grpc::CompletionQueue* cq, Sessions::Stub* service)
     : cq_(cq)
-    , context_(context)
     , service_(service) {
 
     }
 
-  void DetectIntent(int sampleRate, int16_t* buffer, size_t bufferSize) {
+  void DetectIntent(int sampleRate, char* buffer, size_t bufferSize) {
     request_.set_session("projects/baxter-dafd4/agent/sessions/123");
 
     auto* input = request_.mutable_query_input();
 
-    // InputAudioConfig* audioConfig = input->mutable_audio_config();
-    // audioConfig->set_language_code("en-GB");
-    // audioConfig->set_sample_rate_hertz(sampleRate);
-    // audioConfig->set_audio_encoding(AudioEncoding::AUDIO_ENCODING_LINEAR_16);
-    // request_.set_input_audio((char*)buffer, bufferSize);
+    InputAudioConfig* audioConfig = input->mutable_audio_config();
+    audioConfig->set_language_code("en-GB");
+    audioConfig->set_sample_rate_hertz(sampleRate);
+    audioConfig->set_audio_encoding(AudioEncoding::AUDIO_ENCODING_LINEAR_16);
+    request_.set_input_audio(buffer, bufferSize);
 
-    TextInput* textInput = input->mutable_text();
-    textInput->set_text("Open Chrome");
-    textInput->set_language_code("en-GB");
+    // TextInput* textInput = input->mutable_text();
+    // textInput->set_text("Seattle");
+    // textInput->set_language_code("en-GB");
 
-    auto streamer = service_->AsyncDetectIntent(context_, request_, cq_);
+    auto streamer = service_->AsyncDetectIntent(&context_, request_, cq_);
 
     streamer->Finish(&response_, &status_, this);
   }
@@ -52,7 +55,7 @@ public:
 private:
 
   grpc::CompletionQueue* cq_;
-  grpc::ClientContext* context_;
+  grpc::ClientContext context_;
   Sessions::Stub* service_;
 
   DetectIntentRequest request_;
@@ -78,6 +81,9 @@ int main() {
   int numChannels = hotword.numChannels();
   int sampleRate = hotword.sampleRate();
 
+  SnowboyVad vad("./resources/common.res");
+  vad.SetAudioGain(1.0f);
+
   PortAudioSystem portAudio;
 
   portAudio.createStream(
@@ -90,17 +96,6 @@ int main() {
 
   DetectIntentRequest request;
   request.set_session("projects/baxter-dafd4/agent/sessions/123");
-  // TextInput* textInput = input->mutable_text();
-  // textInput->set_text("Open Chrome");
-  // textInput->set_language_code("en-GB");
-  // request.set_input_audio((char*)buffer.data(), buffer.size());
-  // DetectIntentResponse response;
-  grpc::ClientContext context;
-  // grpc::Status status = service->DetectIntent(&context, request, &response);
-  // std::clog << status.ok() << " " << status.error_code() << " " << status.error_message() << std::endl;
-  // auto queryResult = response.query_result();
-  // auto fulfillmentText = queryResult.fulfillment_text();
-  // std::clog << fulfillmentText << std::endl;
 
   grpc::CompletionQueue cq;
 
@@ -112,31 +107,63 @@ int main() {
   deadline.tv_nsec = 0;
   deadline.clock_type = GPR_TIMESPAN;
 
+  std::queue<std::vector<int16_t>> speechBuffer;
+
   while(true) {
     portAudio.read(&buffer);
     if (buffer.size() != 0) {
+      int vadResult = vad.RunVad(buffer.data(), buffer.size());
+      if (vadResult == 0) {
+        speechBuffer.push(buffer);
+      } else {
+        std::queue<std::vector<int16_t>> empty;
+        std::swap(speechBuffer, empty);
+      }
+      std::clog << vadResult << std::endl;
+
       int detectResult = hotword.detect(buffer.data(), buffer.size());
       if (detectResult == 1) {
         std::clog << "hotword detected" << std::endl;
-        SpeechCall* speechCall = new SpeechCall(&cq, &context, service.get());
+        SpeechCall* speechCall = new SpeechCall(&cq, service.get());
         std::clog << speechCall << std::endl;
-        speechCall->DetectIntent(sampleRate, buffer.data(), buffer.size());
+
+        std::clog << "combining " << speechBuffer.size() << std::endl;
+
+        std::vector<int16_t> fullSpeechBuffer;
+
+        while (!speechBuffer.empty()) {
+          auto sample = speechBuffer.front();
+          fullSpeechBuffer.insert(fullSpeechBuffer.end(), sample.begin(), sample.end());
+          speechBuffer.pop();
+        }
+
+        speechCall->DetectIntent(sampleRate, (char*)fullSpeechBuffer.data(), fullSpeechBuffer.size() * sizeof(int16_t));
+
+        SndfileHandle outfile("/tmp/buffer.wav", SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, numChannels, sampleRate);
+        outfile.write(fullSpeechBuffer.data(), fullSpeechBuffer.size());
       }
     }
     bool ok = false;
     void* tag;
-    cq.AsyncNext(&tag, &ok, deadline);
-    if (ok && tag) {
+    auto result = cq.AsyncNext(&tag, &ok, deadline);
+    bool gotEvent = result == grpc::CompletionQueue::GOT_EVENT;
+    if (ok && gotEvent && tag) {
       std::clog << "got response" << std::endl;
-      std::clog << tag << std::endl;
       auto speechCall = reinterpret_cast<SpeechCall*>(tag);
       auto response = speechCall->response();
 
       auto queryResult = response->query_result();
-      // auto queryText = queryResult.query_text();
-      // std::clog << queryText << std::endl;
-      // auto fulfillmentText = queryResult.fulfillment_text();
-      // std::clog << fulfillmentText << std::endl;
+      auto queryText = queryResult.query_text();
+      std::clog << "Query: " << queryText << std::endl;
+      auto fulfillmentText = queryResult.fulfillment_text();
+      std::clog << "Fulfillment: " << fulfillmentText << std::endl;
+      auto languageCode = queryResult.language_code();
+      std::clog << "Language Code: " << languageCode << std::endl;
+      auto intent = queryResult.intent();
+      auto intentName = intent.name();
+      std::clog << "Intent: " << intentName << std::endl;
+      delete speechCall;
+
     }
   }
 
