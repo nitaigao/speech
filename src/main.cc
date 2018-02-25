93564bc38c360bc373595f88b7969418dab331c0
Nicholas Kostelnik <nkostelnik@gmail.com>
2018-02-25 16:42:02 +0800

    starting on async properly

--- src/main.cc
+++ src/main.cc
@@ -14,6 +14,53 @@
 using namespace google::cloud::dialogflow::v2beta1;
 using namespace std::chrono;
 
+class SpeechCall {
+
+public:
+
+  SpeechCall(grpc::CompletionQueue* cq, grpc::ClientContext* context, Sessions::Stub* service)
+    : cq_(cq)
+    , context_(context)
+    , service_(service) {
+
+    }
+
+  void DetectIntent(int sampleRate, int16_t* buffer, size_t bufferSize) {
+    request_.set_session("projects/baxter-dafd4/agent/sessions/123");
+
+    auto* input = request_.mutable_query_input();
+
+    // InputAudioConfig* audioConfig = input->mutable_audio_config();
+    // audioConfig->set_language_code("en-GB");
+    // audioConfig->set_sample_rate_hertz(sampleRate);
+    // audioConfig->set_audio_encoding(AudioEncoding::AUDIO_ENCODING_LINEAR_16);
+    // request_.set_input_audio((char*)buffer, bufferSize);
+
+    TextInput* textInput = input->mutable_text();
+    textInput->set_text("Open Chrome");
+    textInput->set_language_code("en-GB");
+
+    auto streamer = service_->AsyncDetectIntent(context_, request_, cq_);
+
+    streamer->Finish(&response_, &status_, this);
+  }
+
+  DetectIntentResponse* response() {
+    return &response_;
+  }
+
+private:
+
+  grpc::CompletionQueue* cq_;
+  grpc::ClientContext* context_;
+  Sessions::Stub* service_;
+
+  DetectIntentRequest request_;
+  DetectIntentResponse response_;
+  grpc::Status status_;
+
+};
+
 int main() {
 
   auto creds = grpc::GoogleDefaultCredentials();
@@ -43,14 +90,9 @@ int main() {
 
   DetectIntentRequest request;
   request.set_session("projects/baxter-dafd4/agent/sessions/123");
-  auto* input = request.mutable_query_input();
-  // InputAudioConfig* audioConfig = input->mutable_audio_config();
-  // audioConfig->set_language_code("en-GB");
-  // audioConfig->set_sample_rate_hertz(sampleRate);
-  // audioConfig->set_audio_encoding(AudioEncoding::AUDIO_ENCODING_LINEAR_16);
-  TextInput* textInput = input->mutable_text();
-  textInput->set_text("Open Chrome");
-  textInput->set_language_code("en-GB");
+  // TextInput* textInput = input->mutable_text();
+  // textInput->set_text("Open Chrome");
+  // textInput->set_language_code("en-GB");
   // request.set_input_audio((char*)buffer.data(), buffer.size());
   // DetectIntentResponse response;
   grpc::ClientContext context;
@@ -61,11 +103,6 @@ int main() {
   // std::clog << fulfillmentText << std::endl;
 
   grpc::CompletionQueue cq;
-  auto streamer = service->AsyncDetectIntent(&context, request, &cq);
-  grpc::Status status;
-
-  DetectIntentResponse response;
-  streamer->Finish(&response, &status, (void*)1);
 
   std::vector<int16_t> buffer;
 
@@ -80,27 +117,26 @@ int main() {
     if (buffer.size() != 0) {
       int detectResult = hotword.detect(buffer.data(), buffer.size());
       if (detectResult == 1) {
-
+        std::clog << "hotword detected" << std::endl;
+        SpeechCall* speechCall = new SpeechCall(&cq, &context, service.get());
+        std::clog << speechCall << std::endl;
+        speechCall->DetectIntent(sampleRate, buffer.data(), buffer.size());
       }
     }
     bool ok = false;
-    // Tag* tag = nullptr;
-    void* got_tag;
-    cq.AsyncNext(&got_tag, &ok, deadline);
-    if (ok && got_tag == (void*)1) {
-      auto queryResult = response.query_result();
-      auto fulfillmentText = queryResult.fulfillment_text();
-      std::clog << fulfillmentText << std::endl;
-      // std::cout << tag->name << " completed." << std::endl;
-      // tag->happening_now = false;
-      // // if (tag != &create_stream) {
-      // //   std::cerr << "Expected create_stream in cq." << std::endl;
-      // //   return -1;
-      // // }
-      // if (!ok) {
-      //   std::cerr << "Stream closed while creating it." << std::endl;
-      //   return -1;
-      // }
+    void* tag;
+    cq.AsyncNext(&tag, &ok, deadline);
+    if (ok && tag) {
+      std::clog << "got response" << std::endl;
+      std::clog << tag << std::endl;
+      auto speechCall = reinterpret_cast<SpeechCall*>(tag);
+      auto response = speechCall->response();
+
+      auto queryResult = response->query_result();
+      // auto queryText = queryResult.query_text();
+      // std::clog << queryText << std::endl;
+      // auto fulfillmentText = queryResult.fulfillment_text();
+      // std::clog << fulfillmentText << std::endl;
     }
   }
 
